0d77a27a0a31db2db24a01a2e9e01838cc092a39
OLUBI JOHN <olubijohn4live@gmail.com>
2023-09-15 07:58:08 -0700

    INITIAL

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Disclaimer For Guarantyresources</h1>
+      <p> 
+If you need more information or have questions about our site’s disclaimer, please feel free to contact us by email at guarantyresources@gmail.com
+Disclaimers for guaranty resources
+All the information, articles and posts made on this website i.e www.guarantyresources.com, is published in good faith and for general information purposes only. Guaranty resources do NOT make any warranties about the accuracy, completeness, reliability and dependability of the information. Any action taken based on the information you find on this website (guarantyresources), is strictly at your discretion and risk. Guarantyresources will not be liable for any losses and/or damages in connection with the use of our website
+Also note that our website may contain hyperlinks that can be used to visit other external sites. However, as we strive to provide links to useful, ethical and high quality resources, we have no control whatsoever, over the content, nature and use of these websites. Furthermore, links provided to other websites do not imply a recommendation for all the content found on those sites. Site owners and content may change without prior notice and may occur before we have the opportunity to remove a link that may have gone ‘bad’ or ‘untrusted”.
+Consent 
+By using our website, you hereby consent to our disclaimer and agree to our terms and conditions. 
+</p>
     </div>
   );
 }
